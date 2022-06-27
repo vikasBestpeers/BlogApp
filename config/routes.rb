0c6f3915b5a27d92849aad136614dc75dbbeb0df
vikasBestpeers <vsisodiya@bestpeers.com>
2022-06-27 15:41:08 +0530

    Added my project

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+# Rails.application.routes.draw do
+#   root "articles#index"
+
+#   get "/articles", to: "articles#index"
+#   get "/articles/:id", to: "articles#show"
+# end
+
+
+Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles
+end
