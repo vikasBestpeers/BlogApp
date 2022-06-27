2a1c3decaa6714181919926cdeac4659548cf20c
vikasBestpeers <vsisodiya@bestpeers.com>
2022-06-27 18:36:22 +0530

    the blog application is completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+
   def index
     @articles = Article.all
   end
@@ -21,8 +25,30 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
+  
   private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@
 #   get "/articles", to: "articles#index"
 #   get "/articles/:id", to: "articles#show"
 # end
-
-
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
